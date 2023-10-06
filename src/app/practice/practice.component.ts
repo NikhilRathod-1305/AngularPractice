876fe54cd3188068a1488f0d218c075b75243298
NIKHIL\Asus <nikhilrathod@doshaheen.com>
2023-10-06 15:29:23 +0530

    disabling table in staging mode

--- src/app/practice/practice.component.ts
+++ src/app/practice/practice.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { environment } from './../../environments/environment';
 
 @Component({
   selector: 'app-practice',
   templateUrl: './practice.component.html',
   styleUrls: ['./practice.component.scss'],
 })
-export class PracticeComponent {}
+export class PracticeComponent {
+  enableTable: boolean = environment.enableTable;
+}
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,5 +3,5 @@ export const environment = {
   development: false,
   staging: true,
   apiUrl: 'https://stage.api.com',
-  enableTable: true,
+  enableTable: false,
 };
