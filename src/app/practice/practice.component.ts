ce9ed99999e93c50f630f94dc0d5d66e8e3a8d97
NIKHIL\Asus <nikhilrathod@doshaheen.com>
2023-10-06 15:05:24 +0530

    env changes

--- src/app/practice/practice.component.ts
+++ src/app/practice/practice.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { environment } from 'src/environments/environment';
-
 
 @Component({
   selector: 'app-practice',
   templateUrl: './practice.component.html',
-  styleUrls: ['./practice.component.scss']
+  styleUrls: ['./practice.component.scss'],
 })
-export class PracticeComponent {
-  enableTable: boolean = environment.enableTable;
-}
+export class PracticeComponent {}
